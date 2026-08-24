0e5c285d30112cfa962eae309efbc390aaeb941c
Taco <mykolas.raibys@gmail.com>
2026-08-24 16:46:34 +0300

    FIX: fixed an issue where .docx files werent recognised as document files

--- backend/src/cv/cv.service.ts
+++ backend/src/cv/cv.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable  @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return */
 import { Injectable, BadRequestException } from '@nestjs/common';
 import mammoth from 'mammoth';
 
@@ -6,6 +7,8 @@ const { PDFParse } = require('pdf-parse');
 @Injectable()
 export class CvService {
   async extractText(file: Express.Multer.File): Promise<string> {
+    console.log('Received mimetype:', file.mimetype);
+    console.log('Original filename:', file.originalname);
     if (file.mimetype === 'application/pdf') {
       const parser = new PDFParse({ data: file.buffer });
       try {
@@ -18,7 +21,8 @@ export class CvService {
 
     if (
       file.mimetype ===
-      'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        // eslint-disable-next-line prettier/prettier
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' || file.originalname.toLowerCase().endsWith('.docx')
     ) {
       const { value } = await mammoth.extractRawText({ buffer: file.buffer });
       return value;
